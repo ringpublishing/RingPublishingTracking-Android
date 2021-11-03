81e17338bb5690848c847d59aa5c027f17005034
Grzegorz Małopolski <Grzegorz.Malopolski@ringieraxelspringer.pl>
2021-11-03 16:22:04 +0100

    Update unused mocks.

--- RingPublishingTracking/src/test/java/RingPublishingTrackingTest.kt
+++ RingPublishingTracking/src/test/java/RingPublishingTrackingTest.kt
@@ -7,19 +7,14 @@ import android.content.pm.PackageManager
 import android.content.res.Configuration
 import android.content.res.Resources
 import android.hardware.display.DisplayManager
-import android.util.DisplayMetrics
 import android.view.Display
 import android.view.WindowManager
 import com.ringpublishing.tracking.data.RingPublishingTrackingConfiguration
 import com.ringpublishing.tracking.delegate.RingPublishingTrackingDelegate
-import com.ringpublishing.tracking.internal.api.ApiClient
 import com.ringpublishing.tracking.internal.log.Logger
-import com.ringpublishing.tracking.internal.service.ApiService
-import com.ringpublishing.tracking.internal.service.EventsService
 import io.mockk.MockKAnnotations
 import io.mockk.every
 import io.mockk.impl.annotations.MockK
-import io.mockk.mockkStatic
 import org.junit.Before
 import org.junit.Test
 import java.net.URL
@@ -55,9 +50,6 @@ internal class RingPublishingTrackingTest
 	lateinit var apiUrl: URL
 
 	@MockK
-	lateinit var displayMetrics: DisplayMetrics
-
-	@MockK
 	lateinit var displayManager: DisplayManager
 
 	@MockK
@@ -81,9 +73,6 @@ internal class RingPublishingTrackingTest
 		every { context.resources.configuration } returns configuration
 
 		every { displayManager.getDisplay(Display.DEFAULT_DISPLAY) } returns display
-
-		mockkStatic(Resources::class)
-		every { Resources.getSystem().displayMetrics } returns displayMetrics
 
 		every { context.getSharedPreferences(any(), any()) } returns sharedPreferences
 
