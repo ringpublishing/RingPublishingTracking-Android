7b7bfbe0a04438ce45c8bd2b81070154d80aa966
Arkadiusz Wochniak <arkadiusz.wochniak@ringieraxelspringer.pl>
2022-01-26 16:47:12 +0100

    Add description for tracking identifier error

--- RingPublishingTracking/src/main/java/com/ringpublishing/tracking/TrackingIdentifierError.kt
+++ RingPublishingTracking/src/main/java/com/ringpublishing/tracking/TrackingIdentifierError.kt
@@ -3,5 +3,12 @@ package com.ringpublishing.tracking
 enum class TrackingIdentifierError
 {
 
-	REQUEST_ERROR, RESPONSE_ERROR, CONNECTION_ERROR
+	/// There was networking problem during tracking identifier request
+	REQUEST_ERROR,
+
+	/// Backend had problem to process tracking identifier request and returned unexpected response
+	RESPONSE_ERROR,
+
+	/// Problem with connection was occurred
+	CONNECTION_ERROR
 }
