086ab69ca631207fadfd0b945395a0d75e6fc5b8
Arkadiusz Wochniak <arkadiusz.wochniak@ringieraxelspringer.pl>
2023-09-06 14:52:04 +0200

    Static analysis + test fixes

--- demo/src/main/java/com/ringpublishing/tracking/demo/adapter/MainPagerAdapter.kt
+++ demo/src/main/java/com/ringpublishing/tracking/demo/adapter/MainPagerAdapter.kt
@@ -6,7 +6,6 @@
 
 package com.ringpublishing.tracking.demo.adapter
 
-import androidx.annotation.Nullable
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
@@ -16,10 +15,8 @@ class MainPagerAdapter(supportFragmentManager: FragmentManager) : FragmentPagerA
 
 	private val data = mutableListOf<Pair<String, Fragment>>()
 
-	@Nullable
 	override fun getItem(position: Int) = data[position].second
 
-	@Nullable
 	override fun getPageTitle(position: Int) = data[position].first
 
 	override fun getCount() = data.size
