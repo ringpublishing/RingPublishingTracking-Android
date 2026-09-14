def7368d8d4c3c20ff15e12db051c650f4e6219d
Marcin Nowacki <Marcin.Nowacki@ext.ringieraxelspringer.pl>
2026-09-14 15:23:50 +0200

    Fix some code formatting issues

--- RingPublishingTracking/src/main/java/com/ringpublishing/tracking/internal/ConfigurationManager.kt
+++ RingPublishingTracking/src/main/java/com/ringpublishing/tracking/internal/ConfigurationManager.kt
@@ -99,15 +99,11 @@ internal class ConfigurationManager
         return if (ringPublishingTrackingConfiguration.applicationAdvertisementSite.isNullOrEmpty())
         {
             currentAdvertisementArea ?: ""
-        }
-        else
-        {
+        } else {
             if (currentAdvertisementArea.isNullOrEmpty())
             {
                 "${ringPublishingTrackingConfiguration.applicationAdvertisementSite}"
-            }
-            else
-            {
+            } else {
                 "${ringPublishingTrackingConfiguration.applicationAdvertisementSite}/$currentAdvertisementArea"
             }
         }
@@ -118,13 +114,12 @@ internal class ConfigurationManager
 	fun getFullStructurePath(): String
 	{
 		with(ringPublishingTrackingConfiguration)
-		{   if (applicationAdvertisementSite.isNullOrEmpty())
+		{
+            if (applicationAdvertisementSite.isNullOrEmpty())
             {
                 val rootPath = if (applicationRootPath.endsWith("/")) applicationRootPath.removeSuffix("/") else applicationRootPath
                 return currentStructurePath.joinToString("/", "$rootPath${Constants.defaultRootPathSuffixDV}/").lowercase().replace(".", "_")
-            }
-            else
-            {
+            } else {
                 return currentStructurePath.joinToString("/", "$applicationAdvertisementSite/").lowercase().replace(".", "_")
             }
 		}
